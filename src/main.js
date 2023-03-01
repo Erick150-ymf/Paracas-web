171fb21bbe681eb7a509fd4bc33b6be8a29158a6
Erick <yusske06@gmail.com>
2023-02-28 19:56:56 -0500

    Modificaciónes y avance

--- src/main.js
+++ src/main.js
@@ -17,22 +17,3 @@ closeModal.addEventListener('click', () => {
 function showPolicy() {
     policywnd = window.open('http://www.parkingcrew.net/privacy.html', 'pcrew_policy', 'width=890,height=330,left=200,top=200,menubar=no,status=yes,toolbar=no'); policywnd.focus();
 }
-
-// mapa
-
-function initMap() {
-    // The location of Uluru
-    const uluru = { lat: -13.830900581281764, lng: -76.24657158154407 };
-    // The map, centered at Uluru
-    const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 17,
-        center: uluru,
-    });
-        // The marker, positioned at Uluru
-    const marker = new google.maps.Marker({
-        position: uluru,
-        map: map,
-    });
-}
-
-window.initMap = initMap;
---
+++ src/nosotros/map.js
@@ -0,0 +1,6 @@
+// button dirige a google maps
+const map = document.getElementById('map');
+
+map.addEventListener('click', function() {
+    location.href = "https://www.google.com/maps/place/13%C2%B049'53.0%22S+76%C2%B014'48.7%22W/@-13.8314309,-76.2466819,19z/data=!4m4!3m3!8m2!3d-13.831383!4d-76.24686!5m1!1e4"
+});
